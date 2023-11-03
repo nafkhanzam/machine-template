362b251c8185d58f3207b38878c9a481ba8de3f6
nafkhanzam <nafkhanalzamzami@gmail.com>
2023-11-03 14:12:32 +0700

    fix test dir

--- tests/shared.ts
+++ tests/shared.ts
@@ -3,8 +3,10 @@ import * as fs from "https://deno.land/std@0.205.0/fs/mod.ts";
 export const TEST_ENV_DIR = `.test-env`;
 
 export function init(): void {
-  fs.emptyDirSync(TEST_ENV_DIR);
-  Deno.chdir(TEST_ENV_DIR);
+  if (!Deno.cwd().endsWith(TEST_ENV_DIR)) {
+    fs.emptyDirSync(TEST_ENV_DIR);
+    Deno.chdir(TEST_ENV_DIR);
+  }
 }
 
 export function cleanup(rootDir: string): void {
