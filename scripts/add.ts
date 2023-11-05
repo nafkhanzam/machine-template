76fc5c9575c794e039f452c38d0ea06a9a9db49d
nafkhanzam <nafkhanalzamzami@gmail.com>
2023-11-05 21:48:16 +0700

    remove same dir requirements

--- scripts/add.ts
+++ scripts/add.ts
@@ -1,15 +1,8 @@
 //~ deno run -A add.ts <file-path>
 
 import * as assert from "https://deno.land/std@0.205.0/assert/mod.ts";
-import * as path from "https://deno.land/std@0.205.0/path/mod.ts";
 import { add } from "./add-func.ts";
 
-const cwd = Deno.cwd();
-assert.assertEquals(
-  cwd,
-  path.resolve(path.dirname(path.fromFileUrl(import.meta.url))),
-  `You have to run in the same working directory as the add script!`
-);
 assert.assertEquals(
   1,
   Deno.args.length,
